296bf441c914239995e97a7434d9bdd3b33bbf44
Kevin Bertin <kevinbertinpro@gmail.com>
2024-12-16 12:54:16 +0100

    Adding animation with react-spring

--- src/components/ResultElement.tsx
+++ src/components/ResultElement.tsx
@@ -1,15 +1,28 @@
+import { useSpring, animated } from "@react-spring/web";
 interface ResultElementProps {
     data: number | null;
     unit: string;
 }
 
-export const ResultElement = ({ data, unit }: ResultElementProps) => {
+export const ResultElement: React.FC<ResultElementProps> = ({ data, unit }) => {
+    const [springProps] = useSpring(
+        () => ({
+            number: data ?? 0, // Anime vers la valeur cible, 0 par défaut
+            from: { number: 0 }, // Point de départ à 0
+            config: { duration: 750 }, // Durée de l'animation
+        }),
+        [data] // Dépendance pour réanimer si data change
+    );
+
     return (
-        <h2 className="font-bold text-5xl lg:text-6xl italic">
-            <span className="text-[#8151f9]">
-                {data === null ? "--" : data}
-            </span>{" "}
-            {unit}
-        </h2>
+        <div className="flex gap-2">
+            <h2 className="font-bold text-6xl italic">
+                <animated.span className={`text-[#8151f9]`}>
+                    {/* Affiche la valeur entière en animant */}
+                    {data === null ? "--" : springProps.number.to((n) => Math.floor(n))}
+                </animated.span>{" "}
+                {unit}
+            </h2>
+        </div>
     );
 };
